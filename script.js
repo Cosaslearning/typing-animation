9dfd8d3450dab12563fe308bd69656691badab5d
Cosas Learning <cosaslearning@gmail.com>
2024-01-22 15:42:08 +0530

    Add Files

---
+++ script.js
@@ -0,0 +1,7 @@
+/* --------------- Typing Animations  --------------------- */
+var typed = new Typed(".input", {
+    strings: ["राम सिया राम सिया राम,", "जय जय राम,", "राम सिया राम सिया राम,", "जय जय राम॥"],
+    typeSpeed:100,
+    backSpeed: 100,
+    loop: true
+});
